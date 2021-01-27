943dee3feb88977411ad1f6322fc95f18b2e2198
Sek Pulsukh <sek.pulsukh@gmail.com>
2021-01-27 23:42:55 +0700

    navbar responsive top test

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function myFunction2() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
   } else {
-    x.style.display = "block";
+    x.className = "navbar";
   }
 }
