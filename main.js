18d21cb705e1768ad314ba1931258a39831c5923
Sek Pulsukh <sek.pulsukh@gmail.com>
2021-01-27 23:48:48 +0700

    mobile

--- main.js
+++ main.js
@@ -16,3 +16,13 @@ function myFunction() {
     x.className = "navbar";
   }
 }
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
